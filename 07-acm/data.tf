20edb09f62e539e4c2c498b929479a98a34b8267
Nagamani229 <nagamanipotu56@gmail.com>
2025-01-14 20:50:23 +0530

    robo-shop-dev

---
+++ 07-acm/data.tf
@@ -0,0 +1,3 @@
+data "aws_route53_zone" "devopstraining" {
+  name         = "devopstraining.space"
+}
---
+++ 14-payment/variables.tf
@@ -0,0 +1,32 @@
+variable "common_tags" {
+  default = {
+    Project     = "roboshop"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "tags" {
+  default = {
+    Component = "payment"
+  }
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "devopstraining.space"
+}
+variable "iam_instance_profile" {
+  default = "ShellScriptRoleForRoboshop"
+}
+
+variable "app_version" {
+  description = "The version of the application to deploy"
+  type        = string
+}
