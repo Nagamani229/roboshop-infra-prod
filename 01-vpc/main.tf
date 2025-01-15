8187e0339b3c038d1eea7c286f7b33ed1f864dd9
Nagamani229 <nagamaniyadav229@gmail.com>
2025-01-15 07:54:23 +0530

    roboshop-u=infra-dev

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,7 +1,7 @@
 module "roboshop" {
-  #source = "../terraform-aws-vpc"
+  source = "../terraform-aws-vpc"
   #source = "git::https://github.com/Nagamani229/terraform-aws-vpc.git?ref=main"
-  source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
+  #source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
   project_name = var.project_name
   environment = var.environment
   common_tags = var.common_tags
