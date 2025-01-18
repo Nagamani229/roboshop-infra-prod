5c263c2fbdd2ae315c110b3ddc81f869d960f4c9
Nagamani229 <nagamaniyadav229@gmail.com>
2025-01-18 19:52:50 +0530

    catalogue

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,6 +1,6 @@
 module "roboshop" {
   source = "../terraform-aws-vpc"
-  source = "git::https://github.com/Nagamani229/terraform-aws-vpc.git?ref=main"
+  #source = "git::https://github.com/Nagamani229/terraform-aws-vpc.git?ref=main"
   #source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
   project_name = var.project_name
   environment = var.environment
